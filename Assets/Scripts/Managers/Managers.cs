eebee77cb2b78abee449c78633d54c82dfb43d07
mut1996 <mut02130317@gmail.com>
2022-08-19 17:52:43 +0900

    MapDesign
    map

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    private static Managers s_instnace;
+    private static Managers Instance { get { Init(); return s_instnace; } }
+
+    private static void Init() 
+    {
+        if (s_instnace == null) 
+        {
+            GameObject go = GameObject.Find("@Managers");
+
+            if (go == null) 
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instnace = go.GetComponent<Managers>();
+        
+        }
+        
+        
+    } 
+}
