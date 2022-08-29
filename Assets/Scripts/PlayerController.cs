8093f23e181762e23f66009d79507607a4f49744
mut1996 <mut1996@users.noreply.github.com>
2022-08-29 10:58:22 +0900

    move
    move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+
+    private Animator animator;
+    private SpriteRenderer sprite;
+    private Rigidbody2D rigidbody;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+        AnimationCotroll();
     }
+
+
+    // 왼쪽 클릭하면 왼쪽보고 오른쪽 클릭하면 오른쪽봄
+    // 그리고 왼쪽 오른쪽이 아니면 보는 방향 유지.
+    private void Move()
+    {
+        Vector2 dir = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir.x = -1;
+            sprite.flipX = true;
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            dir.x = 1;
+            sprite.flipX = false; 
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir.y = 1;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            dir.y = -1;
+        }
+
+        dir.Normalize();
+        rigidbody.velocity = speed * dir ;
+
+    }
+
+    private void AnimationCotroll() 
+    {
+        if (Mathf.Abs(rigidbody.velocity.magnitude) >= 0.01f)
+        {
+            animator.SetBool("move", true);
+            animator.SetBool("idle", false);
+
+        }
+        else
+        {
+            animator.SetBool("move", false);
+            animator.SetBool("idle", true);
+
+        }
+    }
+
+
+
+
 }
--- Assets/Scripts/Utill/LayerTrigger.cs
+++ Assets/Scripts/Utill/LayerTrigger.cs
@@ -14,8 +14,8 @@ namespace Cainos.PixelArtTopDown_Basic
         private void OnTriggerExit2D(Collider2D other)
         {
             other.gameObject.layer = LayerMask.NameToLayer(layer);
-
             other.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = sortingLayer;
+            
             SpriteRenderer[] srs = other.gameObject.GetComponentsInChildren<SpriteRenderer>();
             foreach ( SpriteRenderer sr in srs)
             {
