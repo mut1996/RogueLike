01875232aed0401b764aec7fbd0953bd59c637a8
mut1996 <mut02130317@gmail.com>
2022-09-07 17:57:54 +0900

    summary
    summary

--- Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
+++ Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Player component statemachine
+/// Idle, Dash, skill, Die, Move, Attack  ->  이번주 까지 
+/// 적 Idle, Attack, Die -> 이번주 까지 
+/// Invetory, Weapon, Inventory -> 다음주 까지 
+/// </summary>
+
 public class Player : MonoBehaviour
 {
     #region State Variables
     public PlayerStateMachine stateMachine { get; private set; }
 
+    public PlayerIdleState IdleState { get; private set; }
 
 
 
@@ -29,9 +38,10 @@ public class Player : MonoBehaviour
 
     private Vector2 workSpace;
 
+
     private void Awake()
     {
-          
+        IdleState = new PlayerIdleState(this, stateMachine, playerData, "Idle");
     }
 
     private void Start()
